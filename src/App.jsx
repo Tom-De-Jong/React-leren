01656b56dcd401259dae0982bf54d558dfc3db20
Tom <40141@ma-web.nl>
2026-02-12 12:21:29 +0100

    started project and added all files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { Foto } from "./assets/aboutMe";
+import { Header } from "./assets/aboutMe";
+import { Paragraph } from "./assets/aboutMe";
+
+function App() {
+return(
+  <>
+  <div className="section">
+  <Foto/>
+  <section className='textSection'>
+
+  
+  <Header/>
+  <Paragraph/>
+  </section>
+  </div>
+  </>
+)
+}
+
+export default App
---
+++ src/assets/aboutMe.jsx
@@ -0,0 +1,20 @@
+export function Foto() {
+    return (
+        <img className="tomPicture" src="https://tomwebsites.nl/tom.png"></img>
+    )
+}
+
+export function Header() {
+    return (
+        <h1 className="header">Mijn naam Tom de Jong</h1>
+    )
+
+}
+
+export function Paragraph() {
+    return(
+    <p className="paragraph">Ik ben tweedejaars student aan het Mediacollege en werk als webdeveloper in Hoorn. Ik richt me op het bouwen van professionele, snelle en gebruiksvriendelijke websites voor bedrijven en ondernemers in Hoorn en omgeving. Dankzij mijn sterke technische achtergrond en zelfstudie kon ik het eerste jaar overslaan en direct starten met uitdagendere projecten.
+
+        Ik ontwikkel moderne websites met een veilige backend, en ik zorg ervoor dat websites sneller, veiliger en beter vindbaar worden in Google. Of het nu gaat om een website laten maken, een Wordpress website, een website redesign, website onderhoud of SEO optimalisatie ik denk mee en lever oplossingen die echt resultaat opleveren. </p>
+    )
+}
